51308790d6fae75991a7ac9a8cd98b61e25fe501
kandicew <wuzhaoqi@grinnell.edu>
2020-03-09 21:12:44 -0500

    change dynamic feature

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
-@result = (1..6).to_a.shuffle.first
+num = (1..6).to_a.shuffle.first
+if num == 1
+@result = "🌸🌸🌸🌸🌸🌸🌸🌸🌸"
+elsif num == 2
+@result = "💐💐💐💐"
+elsif num == 3
+@result = "🌷🌷🌷🌷🌷🌷🌷"
+elsif num == 4
+@result = "🌻🌻🌻🌻🌻"
+elsif num == 5
+@result = "🌹"
+else
+@result = "🌺🌺🌺🌺🌺🌺🌺🌺🌺"
+end
   end
 
   def help
