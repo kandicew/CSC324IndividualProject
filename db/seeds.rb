5c359239a44a393d5894fffeed052618647bc504
kandicew <wuzhaoqi@grinnell.edu>
2020-03-11 15:07:04 -0500

    seed database change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times do |i|
-  Dataset.create(name: "d ##{i}", add: "data")
-end
+
+#5.times do |i|
+  Dataset.create(name: "🍄", add: "data")
+#end
