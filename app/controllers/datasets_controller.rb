58a30e8392256295b2793e0a2f7cdc6248a09b65
kandicew <wuzhaoqi@grinnell.edu>
2020-04-01 17:00:01 -0500

    dataset controller

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -3,4 +3,19 @@ class DatasetsController < ApplicationController
  @dataset = Dataset.new
   end
 
+
+ def create
+    @dataset = Dataset.new(dataset_params)
+    if @dataset.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def dataset_params
+      params.require(:dataset).permit(:name, :add)
+    end
 end
